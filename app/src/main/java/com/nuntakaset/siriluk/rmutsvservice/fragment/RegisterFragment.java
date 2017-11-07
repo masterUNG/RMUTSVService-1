ee913bc76ac64d62680e775456ce2d84b78ef7ee
KwangSrlNtks <kwangsiriluk2722@gmail.com>
2017-11-07 16:42:44 +0700

    Finish 7nov

--- app/src/main/java/com/nuntakaset/siriluk/rmutsvservice/fragment/RegisterFragment.java
+++ app/src/main/java/com/nuntakaset/siriluk/rmutsvservice/fragment/RegisterFragment.java
@@ -3,17 +3,117 @@ package com.nuntakaset.siriluk.rmutsvservice.fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.RadioGroup;
 
 import com.nuntakaset.siriluk.rmutsvservice.R;
+import com.nuntakaset.siriluk.rmutsvservice.RMUTSV;
+import com.nuntakaset.siriluk.rmutsvservice.utility.MyAlert;
 
 /**
  * Created by ACER on 7/11/2560.
  */
 
 public class RegisterFragment extends Fragment{
+//    Explicit
+    private String nameString, userString, passwordString, categoryString;
+    private boolean aBoolean = true;
+
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+//        ToolBar Controller
+        toolBarController();
+
+//        Save Controller
+        saveController();
+
+//        Category Controller
+        categoryController();
+
+
+    }   // Main Method
+
+    private void categoryController() {
+        RadioGroup radioGroup = getView().findViewById(R.id.ragCategory);
+        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup radioGroup, int i) {
+
+                aBoolean = false;
+                switch (i) {
+                    case R.id.radBuyer:
+                        categoryString = "Buyer";
+                        break;
+                    case R.id.radSaleser:
+                    categoryString = "Saleser";
+                        break;
+                }
+
+            }
+        });
+    }
+
+    private void saveController() {
+        ImageView imageView = getView().findViewById(R.id.imvSave);
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+//                Initial View
+                EditText nameEditText = getView().findViewById(R.id.edtName);
+                EditText userEditText = getView().findViewById(R.id.edtUser);
+                EditText passwordEditText = getView().findViewById(R.id.edtPassword);
+
+//                Change Data Type
+                nameString = nameEditText.getText().toString().trim();
+                userString = userEditText.getText().toString().trim();
+                passwordString = passwordEditText.getText().toString().trim();
+
+//                Check Space
+                if (nameString.equals("") || userString.equals("") || passwordString.equals("")) {
+//                    Have Space
+                    MyAlert myAlert = new MyAlert(getActivity());
+                    myAlert.myDialog("Hlave Space",
+                            "Please Fill All Every Blank");
+
+                } else if (aBoolean) {
+//                    Non Choose Choice
+                    MyAlert myAlert = new MyAlert(getActivity());
+                    myAlert.myDialog("Non Choose Category",
+                            "Please Choose Category");
+                } else {
+//                    Choosed Choice
+                }
+
+            }   // On Click
+        });
+    }
+
+    private void toolBarController() {
+        Toolbar toolbar = getView().findViewById(R.id.toolbarRegister);
+        ((RMUTSV)getActivity()).setSupportActionBar(toolbar);
+        ((RMUTSV) getActivity()).getSupportActionBar()
+                .setTitle(getResources().getString(R.string.new_register));
+
+        ((RMUTSV)getActivity()).getSupportActionBar().setHomeButtonEnabled(true);
+        ((RMUTSV)getActivity()).getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                getActivity().getSupportFragmentManager().popBackStack();
+            }
+        });
+
+    }
 
     @Nullable
     @Override
---
+++ app/src/main/java/com/nuntakaset/siriluk/rmutsvservice/utility/MyAlert.java
@@ -0,0 +1,40 @@
+package com.nuntakaset.siriluk.rmutsvservice.utility;
+
+import android.content.Context;
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
+
+import com.nuntakaset.siriluk.rmutsvservice.R;
+
+/**
+ * Created by ACER on 7/11/2560.
+ */
+
+public class MyAlert {
+
+//    Explicit
+    private Context context;
+
+    public MyAlert(Context context) {
+        this.context = context;
+    }
+
+    public void myDialog(String strTitle, String strMessage){
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(context);
+        builder.setCancelable(false);
+        builder.setIcon(R.drawable.ic_action_alert);
+        builder.setTitle(strTitle);
+        builder.setMessage(strMessage);
+        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                dialogInterface.dismiss();
+            }
+        });
+        builder.show();
+
+    }
+
+
+}   // Main Class
