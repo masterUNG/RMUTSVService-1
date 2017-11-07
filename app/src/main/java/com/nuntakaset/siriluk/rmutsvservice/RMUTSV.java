df961a9ff3dabffe221c9a970ae19511faf25043
KwangSrlNtks <kwangsiriluk2722@gmail.com>
2017-11-07 12:03:32 +0700

    before lunce

--- app/src/main/java/com/nuntakaset/siriluk/rmutsvservice/RMUTSV.java
+++ app/src/main/java/com/nuntakaset/siriluk/rmutsvservice/RMUTSV.java
@@ -3,11 +3,23 @@ package com.nuntakaset.siriluk.rmutsvservice;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.nuntakaset.siriluk.rmutsvservice.fragment.MainFragment;
+
 public class RMUTSV extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_rmutsv);
-    }
-}
+
+//        Add Fragment to Activity
+        if (savedInstanceState == null){
+
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.contentFragmentMain, new MainFragment()).commit();
+
+        }
+
+    }   // Main Method
+
+}   // Main Class
